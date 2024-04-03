f3fd16f33de8442eaa027003be7dbeabacb35e79
HaoxuanGong <hgon777@aucklanduni.ac.nz>
2024-04-03 18:05:25 +1300

    initial template

---
+++ backend/MySQLDatabaseServiceNode/src/main/java/uoa/nightingales/mysqldatabaseservicenode/MySqlDatabaseServiceNodeApplication.java
@@ -0,0 +1,13 @@
+package uoa.nightingales.mysqldatabaseservicenode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlDatabaseServiceNodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySqlDatabaseServiceNodeApplication.class, args);
+    }
+
+}
---
+++ backend/MySQLDatabaseServiceNode/src/test/java/uoa/nightingales/mysqldatabaseservicenode/MySqlDatabaseServiceNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package uoa.nightingales.mysqldatabaseservicenode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySqlDatabaseServiceNodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
