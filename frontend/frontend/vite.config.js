f82ca7ee2113821c25c24786938b5f50b4189afb
Xinhuiqiang Xu <xuxinhuiqiang@gmail.com>
2024-04-12 15:26:28 +1200

    fix the bug that the layout is not correct

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3000 // you can specify the port you want to use
+
+  }
 })
