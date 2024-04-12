20793486bfd625ae30365dab843e462188ad54da
Xinhuiqiang Xu <xuxinhuiqiang@gmail.com>
2024-04-12 18:44:42 +1200

    finish the second row of the eplore page

---
+++ frontend/frontend/src/Components/Explore/PictureSecondRow/PictureRow.jsx
@@ -0,0 +1,23 @@
+
+import './PictureRow.css/PictureRow.css'
+import cat1 from '../../../assets/2row2.png'
+import pinkBall from '../../../assets/ball.png'
+import catGalaxy from '../../../assets/catGalaxy.png'
+import parrots from '../../../assets/parrots.png'
+export default function PictureRow() {
+  return (
+    <div className="pictureRow-container">
+        <div>
+            <ul className="leftPicture">
+            <li><img src={cat1} alt="cat" /></li>
+            <li><img src={catGalaxy} alt="" /></li>
+            <li><img src={parrots} alt="" /></li>
+        </ul>  
+        </div>
+        <div className="rightBallPicture">
+            <img src={pinkBall} alt="cat" />
+        </div>
+      
+    </div>
+  )
+}
--- frontend/frontend/src/Pages/ExplorePage/Explore.jsx
+++ frontend/frontend/src/Pages/ExplorePage/Explore.jsx
@@ -3,23 +3,30 @@ import './ExploreCSS/Explore.css'
 import { Outlet } from 'react-router-dom'
 import Ball from '../../Components/Ball/Ball'
 import IntroRow from '../../Components/Explore/IntroRow/IntroRow'
+import PictureRow from "../../Components/Explore/PictureSecondRow/PictureRow"
 export default function Explore() {
   return (
     <div>
         <Ball />
        
         <div className="Explore-container">
-          <IntroRow/>
-        
+          <div>
+              <IntroRow/>
+          </div>
+      
           <div className="pictureRowContainer">
 
+          <PictureRow />
           </div>
+
           <div className="embeddAIContainer">
 
           </div>
+
           <div className="exploreYoutubeCOn">
 
           </div>
+
           <div className="popularTrackContainer">
 
           </div>
