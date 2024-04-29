a0d0bba629983a83ce5bb84f15abe07b8b9dfcd5
jgai284 <jgai284@aucklanduni.ac.nz>
2024-04-29 19:05:31 +1200

    decorative balls added

--- frontend/frontend-react/src/Pages/SpotifyPage/Spotify.jsx
+++ frontend/frontend-react/src/Pages/SpotifyPage/Spotify.jsx
@@ -2,13 +2,15 @@ import "./SpotifyCSS/Spotify.css";
 import SearchTopRow from "../../Components/Spotify/SearchTopRow/SearchTopRow";
 import RecommendationRow from "../../Components/Spotify/RecommendationRow/RecommendationRow";
 import AIRecommendationRow from "../../Components/Spotify/AIRecommendationRow/AIRecommendationRow";
+import SpotifyRow from "../../Components/Spotify/SpotifyRow/SpotifyRow";
+import SubRow from "../../Components/Explore/SubscribeRow/SubscribeRow";
 import BallDynamic from "../../Components/BallDynamic/Ball";
-import SpotifyRow from "../../Components/Explore/SpotifyRow/SpotifyRow";
+import BallStatic from "../../Components/BallStatic/Ball";
 
 export default function Spotify() {
   return (
     <div>
-      {/* <BallDynamic /> */}
+      <BallDynamic />
       <div className="Spotify-container">
         <div>
           <SearchTopRow />
@@ -16,11 +18,18 @@ export default function Spotify() {
         <div>
           <RecommendationRow />
         </div>
-        <div>
+        <div className="AIRecommendation-container">
           <AIRecommendationRow />
+          <BallStatic />
         </div>
         <div>
           <SpotifyRow />
+        </div>
+        <div>
+          <SubRow />
+          <div className="subBall">
+            <BallStatic />
+          </div>
         </div>
       </div>
     </div>
