b55e67ab8a2bf1b50978b4552ed92211b7a515de
HaoxuanGong <hgon777@aucklanduni.ac.nz>
2024-04-30 01:46:33 +1200

    feat: added endpoints on frontend server for the authentication logic flow

---
+++ frontend/frontend-server/routes/music_retrieval/music_authentication.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import { concatenateUrl } from '../../helpers/UrlHelper.js';
+import spotifyConfig from '../../configs/spotify-config.json' assert { type: 'json' };
+import axios from 'axios';
+
+const router = express.Router();
+
+router.get('/token', async (req, res) => {
+    const url = concatenateUrl(spotifyConfig, spotifyConfig.getAccessToken);
+    console.log(url);
+
+    const response = await axios.get(url, { params: req.query });
+
+    // backend server will return 404 if user has not logged in via Spotify
+    if(response.status === 404) {
+        res.status(401).send('Need to login via Spotify');
+    } else {
+        res.json(response.data);
+    }
+});
+
+router.get('/signin', async (req, res) => {
+    const url = concatenateUrl(spotifyConfig, spotifyConfig.redirectToSpotify);
+    console.log(url);
+
+    try{
+        const response = await axios.get(url);
+        res.json(response.data);
+    }catch(error){
+        console.error("Error during retrieving sign in url:", error.response ? error.response.data : error.message);
+        res.status(500).send('Error during retrieving sign in url');
+    }
+    
+});
+
+router.get('/callback', async (req, res) => {
+    const url = concatenateUrl(spotifyConfig, spotifyConfig.handleSpotifyCallback);
+    console.log(url);
+
+    try{
+        // receving the refresh token and access token
+        const response = await axios.get(url, { params: req.query });
+        res.json(response.data);
+        console.log(response.data);
+
+        // saving the refresh token and access token
+        const userId = req.cookies.userId;
+        const { refreshToken, accessToken } = response.data;
+
+        const saveTokenUrl = concatenateUrl(spotifyConfig, spotifyConfig.saveTokens);
+        axios.post(saveTokenUrl, {params: {userId, accessToken, refreshToken}});
+
+        res.redirect('http://localhost:3000/explore');
+
+    }catch(error){
+        console.error("Error during retrieving sign in url:", error.response ? error.response.data : error.message);
+        res.status(500).send('Error during retrieving sign in url');
+    }
+});
+
+export default router;
