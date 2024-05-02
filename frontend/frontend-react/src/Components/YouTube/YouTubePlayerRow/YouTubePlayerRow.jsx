ea40d90f4824581c8919f117d0b467181d577fd1
jgai284 <jgai284@aucklanduni.ac.nz>
2024-05-02 21:53:22 +1200

    player page initialized

---
+++ frontend/frontend-react/src/Components/YouTube/YouTubePlayerRow/YouTubePlayerRow.jsx
@@ -0,0 +1,20 @@
+import "./YouTubePlayerRowCSS/YouTubePlayerRow.css";
+
+export default function YouTubePlayerRow() {
+  return (
+    <div className="YouTubePlayerRow-container">
+      <div className="left">
+        <h1>ww</h1>
+        <h1>ww</h1>
+        <h1>ww</h1>
+        <h1>ww</h1>
+      </div>
+      <div className="right">
+        <h1>ww</h1>
+        <h1>ww</h1>
+        <h1>ww</h1>
+        <h1>ww</h1>
+      </div>
+    </div>
+  );
+}
--- frontend/frontend-react/src/Pages/YouTubePlayerPage/YouTubePlayer.jsx
+++ frontend/frontend-react/src/Pages/YouTubePlayerPage/YouTubePlayer.jsx
@@ -1,12 +1,16 @@
 import "./YouTubePlayerCSS/YouTubePlayer.css";
+import BallDynamic from "../../Components/BallDynamic/Ball";
+import YouTubePlayerRow from "../../Components/YouTube/YouTubePlayerRow/YouTubePlayerRow";
 
 export default function YouTubePlayer() {
   return (
     <div>
-      <h1>ww</h1>
-      <h1>ww</h1>
-      <h1>ww</h1>
-      <h1>ww</h1>
+      {/* <BallDynamic /> */}
+      <div className="YouTubePlayer-container">
+        <div>
+          <YouTubePlayerRow />
+        </div>
+      </div>
     </div>
   );
 }
