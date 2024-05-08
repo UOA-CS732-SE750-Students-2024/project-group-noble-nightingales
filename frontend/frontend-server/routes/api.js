6dc0b3414b6e4b9a05f365593cec8735567dd7b7
HaoxuanGong <hgon777@aucklanduni.ac.nz>
2024-05-08 13:10:14 +1200

    feat: initial set up of express endpoints

--- frontend/frontend-server/routes/api.js
+++ frontend/frontend-server/routes/api.js
@@ -2,11 +2,13 @@ import express from 'express';
 import userRouter from './user_credential/user.js';
 import videoRouter from './video_retrieval/video.js';
 import musicRouter from './music_retrieval/music.js';
+import commentRouter from './comment_retrieval/comment.js';
 
 const router = express.Router()
 
 router.use('/api', userRouter);
 router.use('/api', videoRouter);
 router.use('/api', musicRouter);
+router.use('/api', commentRouter);
 
 export default router
---
+++ frontend/frontend-server/routes/comment_retrieval/retrieve_comment.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/retrieve_comment', (req, res) => {
+    
+})
+
+export default router
+
